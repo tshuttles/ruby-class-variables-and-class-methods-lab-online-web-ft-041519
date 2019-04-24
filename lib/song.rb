655f5d330cb8589287023bf4c7d3370de5737bb6
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-24 01:51:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
     @artist = artist 
     @genre = genre 
     @@count += 1 
-    @@genres += 1 
-    @@artists += 1 
+    @@genres << genre 
+    @@artists << artist
   end 
   
   def self.name(name)
