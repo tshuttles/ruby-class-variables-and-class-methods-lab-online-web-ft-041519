ebeabdc559d432443d70256cc03cba10ebf60633
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-24 02:03:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,11 +40,11 @@ class Song
   
   def self.artist_count
     hash = {} 
-    @@genres.each do |genre| 
-      if hash[genre] 
-        hash[genre] += 1 
+    @@genres.each do |artist| 
+      if hash[artist] 
+        hash[artist] += 1 
       else 
-        hash[genre] = 1 
+        hash[artist] = 1 
       end 
       end 
       return hash 
