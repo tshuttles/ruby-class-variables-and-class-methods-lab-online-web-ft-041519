0d51788e6cedf063a496a7de21eb0ca0002e125e
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-24 02:04:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,37 +5,50 @@ class Song
   @@genres = []
   @@artists = [] 
   
-  def Song.new(name, artist, genre)
-    @name = name 
-    @artist = artist 
-    @genre = genre
-  end 
-  
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist 
     @genre = genre 
     @@count += 1 
-    @@genres += 1 
-    @@artists += 1 
+    @@genres << genre 
+    @@artists << artist
   end 
   
-  def self.name(name)
-    @name = name 
-  end 
-  
-  def self.count(count)
-    @@count = count
+  def self.count
+    @@count
   end 
 
-  def self.genres(genre)
-    @@genres = genre 
+  def self.genres
+    @@genres.uniq 
   end 
 
-  def self.artists(artist)
-    @@artists = artist 
+  def self.artists
+    @@artists.uniq
   end 
   
+  def self.genre_count 
+    hash = {} 
+    @@genres.each do |genre| 
+      if hash[genre] 
+        hash[genre] += 1 
+      else 
+        hash[genre] = 1 
+      end 
+      end 
+      return hash 
+  end 
+  
+  def self.artist_count
+    hash = {} 
+    @@artists.each do |artist| 
+      if hash[artist] 
+        hash[artist] += 1 
+      else 
+        hash[artist] = 1 
+      end 
+      end 
+      return hash 
+  end 
 end 
 
 
