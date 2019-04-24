d5556d1355f14ad405fa4ea03f4ed89900d7f7bf
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-24 01:46:48 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  @@count = 0 
+  @@genres = []
+  @@artists = [] 
+  
+  def Song.new(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+  end 
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1 
+    @@genres += 1 
+    @@artists += 1 
+  end 
+  
+  def self.name(name)
+    @name = name 
+  end 
+  
+  def self.count(count)
+    @@count = count
+  end 
+
+  def self.genres(genre)
+    @@genres = genre 
+  end 
+
+  def self.artists(artist)
+    @@artists = artist 
+  end 
+  
+end 
+
+
+
+
